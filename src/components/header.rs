64e3800405863acdeaf8439a25d65bc0a9732716
Madzong <ytnaintro@gmail.com>
2025-03-23 00:21:34 +0100

    I am very stupid

--- src/components/header.rs
+++ src/components/header.rs
@@ -10,15 +10,15 @@ pub fn Header() -> impl IntoView {
         <style>{STYLE_SHEET}</style>
 
         <header>
-            <a href="/" class=ClassName::SIDENAV>
+            <a href="/bingo-leptos" class=ClassName::SIDENAV>
                 "Oficjalne"
             </a>
             <h1>
-                <a href="/" id="main-text">
+                <a href="/bingo-leptos" id="main-text">
                     <GlitchedText text="BINGO" />
                 </a>
             </h1>
-            <a href="/custom" class=ClassName::SIDENAV>
+            <a href="/bingo-leptos/custom" class=ClassName::SIDENAV>
                 "Customy"
             </a>
         </header>
